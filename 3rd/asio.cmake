529dd75e122c2e3ee2b2cff465006775feee1b3a
spaceape3001 <spaceape3001@users.noreply.github.com>
2024-10-11 13:10:17 +0000

    Lib Rename
    See the toolbox

--- 3rd/asio.cmake
+++ 3rd/asio.cmake
@@ -1,9 +1,9 @@
-if(NOT TARGET asio)
+if(NOT TARGET yq_asio)
     set(asio_dir ${CMAKE_CURRENT_LIST_DIR}/asio/asio)
-    add_library(asio INTERFACE)
-    target_sources(asio INTERFACE
+    add_library(yq_asio INTERFACE)
+    target_sources(yq_asio INTERFACE
         ${asio_dir}/include/asio.hpp
     )
-    target_include_directories(asio INTERFACE ${asio_dir}/include)
+    target_include_directories(yq_asio INTERFACE ${asio_dir}/include)
 endif()
 
